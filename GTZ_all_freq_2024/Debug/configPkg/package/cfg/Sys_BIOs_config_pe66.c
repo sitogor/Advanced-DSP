324311cb4f73b6fe961ddcf44acef661cd95efcd
sitogor <sitogor.02@gmail.com>
2024-03-19 23:31:54 +0000

    report updated

--- GTZ_all_freq_2024/Debug/configPkg/package/cfg/Sys_BIOs_config_pe66.c
+++ GTZ_all_freq_2024/Debug/configPkg/package/cfg/Sys_BIOs_config_pe66.c
@@ -2687,7 +2687,7 @@ __FAR__ const xdc_SizeT ti_sysbios_utils_Load_Module_State_taskList__O = offseto
  *  Define absolute path prefix for this executable's
  *  configuration generated files.
  */
-xdc__META(__ASM__, "@(#)__ASM__ = C:/Users/Tszyau/Desktop/Advanced_DSP_FPGA/Coursework_1/Advanced-DSP/GTZ_all_freq_2024/Debug/configPkg/package/cfg/Sys_BIOs_config_pe66");
+xdc__META(__ASM__, "@(#)__ASM__ = C:/Users/Holyo/Documents/Advanced DSP/Advanced DSP Coursework/Advanced-DSP/GTZ_all_freq_2024/Debug/configPkg/package/cfg/Sys_BIOs_config_pe66");
 
 /*
  *  ======== __ISA__ ========
